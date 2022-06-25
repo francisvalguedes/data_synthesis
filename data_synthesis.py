385012920a68869baa2dc3531f2d981669b78439
francisvalguedes <francisval.guedes.soares.ufrn.edu.br>
2022-06-24 23:41:28 -0300

    add "n_pontos"

--- data_synthesis.py
+++ data_synthesis.py
@@ -3,19 +3,14 @@ import pandas as pd
 import re
 
 txt_files = glob.glob('data/*.txt')
-
 # print(txt_files)
 
-data = {'NORAD_ID':[], 'N':[],  'h0':[], 'd_h0':[],'d_min':[], 'd_fin':[]  }
+data = {'NORAD_ID':[], 'N':[],  'h0':[], 'd_h0':[],'d_min':[], 'd_fin':[], 'n_pontos':[]  }
 
 for file_name in txt_files:
     df = pd.read_csv(file_name)
-    # print(df.head())
     df = df.drop(0)
-    # df1 = df.iloc[[0, df["Range (km)"].idxmin(),-1]]
-    # a = min(df.loc[:,"Range (km)"] )
     norad_id = re.findall(r'[0-9]+', file_name)
-    # print(norad_id)
 
     data['NORAD_ID'].append(norad_id[0])
     data['N'].append(norad_id[1])
@@ -23,8 +18,7 @@ for file_name in txt_files:
     data['d_h0'].append(df.loc[1,"Range (km)"])
     data['d_min'].append(min(df.loc[:,"Range (km)"]))
     data['d_fin'].append(df.iloc[-1]["Range (km)"])
-    # df2 = pd.DataFrame([{'NORAD_ID':array[0], 'N':array[1],  'h0':df.iloc[0]["Range (km)"] , 'd_h0':df.loc[1,"Range (km)"],'d_min':min(df.loc[:,"Range (km)"]), 'd_fin':df.iloc[-1]["Range (km)"]  }])
-
+    data['n_pontos'].append(df.shape[0] )
 
 df_synthesis = pd.DataFrame(data)
 
